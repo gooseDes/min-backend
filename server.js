2b772819d87e67e34f06a34df75be2d61ed2e7d0
gooseDes <sasaprst32@gmail.com>
2025-07-28 19:08:51 +0300

    cors updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*"
+        origin: ["http://localhost:3000", "https://msg-min.xyz"]
     }
 });
 
